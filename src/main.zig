e0db50d40c163dfa21148230c42fba97dacac8fb
fariaiago <111879008+fariaiago@users.noreply.github.com>
2025-04-04 11:04:06 -0300

    final

--- src/main.zig
+++ src/main.zig
@@ -7,9 +7,13 @@ const rgui = @import("raygui");
 const Imagem = struct {
     path: []const u8,
     og: rl.Texture,
-	nn_small: rl.Texture,
+	nn_reduce: rl.Texture,
 	nn_expand: rl.Texture,
+	bilinear_reduce: rl.Texture,
+	bilinear_expand: rl.Texture,
 };
+
+const ImagemList = std.MultiArrayList(Imagem);
 
 var camera = rl.Camera2D{
 	.offset = .{ .x = 540, .y = 360},
@@ -22,7 +26,7 @@ pub fn main() !void {
 	var gpa = std.heap.DebugAllocator(.{}).init;
 	const allocator = gpa.allocator();
 
-	var images = std.MultiArrayList(Imagem){};
+	var images = ImagemList{};
 	defer images.deinit(allocator);
 
 	rl.setConfigFlags(.{ .window_resizable = true });
@@ -39,8 +43,10 @@ pub fn main() !void {
 					try images.append(allocator, .{
 						.path = try allocator.dupeZ(u8, std.mem.span(droppedFiles.paths[i])),
 						.og = tx,
-						.nn_small = try rs.nearest_neighbor_small(&imgToScale),
+						.nn_reduce = try rs.nearest_neighbor_reduce(&imgToScale),
 						.nn_expand = try rs.nearest_neighbor_expand(&imgToScale),
+						.bilinear_reduce = try rs.bilinear_reduce(&imgToScale),
+						.bilinear_expand = try rs.bilinear_expand(&imgToScale),
 					});
 				} else |err| switch (err) {
 					else => {},
@@ -57,13 +63,10 @@ pub fn main() !void {
 		}
 
 		if (rl.isMouseButtonDown(.left)) {
-			camera.target.x -= rl.getMouseDelta().x;
-			camera.target.y -= rl.getMouseDelta().y;
+			camera.target.x -= rl.getMouseDelta().x * (1 / camera.zoom);
+			camera.target.y -= rl.getMouseDelta().y * (1 / camera.zoom);
 		}
-
-		camera.target.x += rl.getMouseWheelMoveV().x * 20000 * rl.getFrameTime() * (1 / camera.zoom);
-		camera.target.y -= rl.getMouseWheelMoveV().y * 20000 * rl.getFrameTime() * (1 / camera.zoom);
-
+		
 		rl.beginDrawing();
 		rl.beginMode2D(camera);
 		defer rl.endMode2D();
@@ -73,10 +76,14 @@ pub fn main() !void {
 
 		var yOffset: i32 = 0;
 		const slice = images.slice();
-		for (slice.items(.og), slice.items(.nn_expand)) |tx, scaled| {
+		for (slice.items(.og), slice.items(.nn_expand), slice.items(.nn_reduce), slice.items(.bilinear_expand), slice.items(.bilinear_reduce))
+				|tx, nn_exp, nn_red, bl_exp, bl_red| {
+			rl.drawTexture(bl_red, 0 - tx.width - nn_red.width - bl_red.width, yOffset, .white);
+			rl.drawTexture(nn_red, 0 - tx.width - nn_red.width, yOffset, .white);
 			rl.drawTexture(tx, 0 - tx.width, yOffset, .white);
-			rl.drawTexture(scaled, 0, yOffset, .white);
-			yOffset += if (tx.height > scaled.height) tx.height else scaled.height;
+			rl.drawTexture(nn_exp, 0, yOffset, .white);
+			rl.drawTexture(bl_exp, nn_exp.width, yOffset, .white);
+			yOffset += nn_exp.height;
 		}
 	}
 }
--- src/resize.zig
+++ src/resize.zig
@@ -1,26 +1,25 @@
 const std = @import("std");
 const rl = @import("raylib");
 
-pub fn nearest_neighbor_small(image: *rl.Image) !rl.Texture {
+pub fn nearest_neighbor_reduce(image: *rl.Image) !rl.Texture {
 	var new = rl.genImageColor(@divFloor(image.width, 2), @divFloor(image.height, 2), .white);
 	
-	var colors = try rl.loadImageColors(new);
-	const bytes_per_pixel : usize = @intCast(rl.getPixelDataSize(1, 1, image.format));
-	const len : usize = @intCast(image.width * image.height * rl.getPixelDataSize(1, 1, image.format));
-	const old_data = @as([*]u8, @ptrCast(image.data))[0..len];
-
+	var new_colors = try rl.loadImageColors(new);
+	const old_colors = try rl.loadImageColors(image.*);
+	defer rl.unloadImageColors(old_colors);
+	
 	const old_width : usize = @intCast(image.width);
 	const new_width : usize = @intCast(new.width);
 	const new_height : usize = @intCast(new.height);
 	for (0..new_width) |i| {
 		for (0..new_height) |j| {
-			colors[j * new_width + i].r = old_data[j * old_width * bytes_per_pixel * 2 + i * 2 * bytes_per_pixel];
-			colors[j * new_width + i].g = old_data[j * old_width * bytes_per_pixel * 2 + (i + 1) * 2 * bytes_per_pixel];
-			colors[j * new_width + i].b = old_data[j * old_width * bytes_per_pixel * 2 + (i + 2) * 2 * bytes_per_pixel];
-			colors[j * new_width + i].a = 255;
+			new_colors[j * new_width + i].r = old_colors[j * old_width * 2 + i * 2].r;
+			new_colors[j * new_width + i].g = old_colors[j * old_width * 2 + i * 2].g;
+			new_colors[j * new_width + i].b = old_colors[j * old_width * 2 + i * 2].b;
+			new_colors[j * new_width + i].a = old_colors[j * old_width * 2 + i * 2].a;
 		}
 	}
-	new.data = colors.ptr;
+	new.data = new_colors.ptr;
 	new.format = .uncompressed_r8g8b8a8;
 
 	return rl.loadTextureFromImage(new);
@@ -44,21 +43,140 @@ pub fn nearest_neighbor_expand(image: *rl.Image) !rl.Texture {
 			new_colors[j * new_width * 2 + i * 2].b = old_colors[j * old_width + i].b;
 			new_colors[j * new_width * 2 + i * 2].a = old_colors[j * old_width + i].a;
 
-			new_colors[j * new_width * 2 + (i + 1) * 2].r = old_colors[j * old_width + i].r;
-			new_colors[j * new_width * 2 + (i + 1) * 2].g = old_colors[j * old_width + i].g;
-			new_colors[j * new_width * 2 + (i + 1) * 2].b = old_colors[j * old_width + i].b;
-			new_colors[j * new_width * 2 + (i + 1) * 2].a = old_colors[j * old_width + i].a;
+			new_colors[j * new_width * 2 + i * 2 + 1].r = old_colors[j * old_width + i].r;
+			new_colors[j * new_width * 2 + i * 2 + 1].g = old_colors[j * old_width + i].g;
+			new_colors[j * new_width * 2 + i * 2 + 1].b = old_colors[j * old_width + i].b;
+			new_colors[j * new_width * 2 + i * 2 + 1].a = old_colors[j * old_width + i].a;
+
+			
+			new_colors[j * new_width * 2 + i * 2 + new_width].r = old_colors[j * old_width + i].r;
+			new_colors[j * new_width * 2 + i * 2 + new_width].g = old_colors[j * old_width + i].g;
+			new_colors[j * new_width * 2 + i * 2 + new_width].b = old_colors[j * old_width + i].b;
+			new_colors[j * new_width * 2 + i * 2 + new_width].a = old_colors[j * old_width + i].a;
+
+			new_colors[j * new_width * 2 + i * 2 + new_width + 1].r = old_colors[j * old_width + i].r;
+			new_colors[j * new_width * 2 + i * 2 + new_width + 1].g = old_colors[j * old_width + i].g;
+			new_colors[j * new_width * 2 + i * 2 + new_width + 1].b = old_colors[j * old_width + i].b;
+			new_colors[j * new_width * 2 + i * 2 + new_width + 1].a = old_colors[j * old_width + i].a;
+		}
+	}
+	new.data = new_colors.ptr;
+	new.format = .uncompressed_r8g8b8a8;
+
+	return rl.loadTextureFromImage(new);
+}
+
+pub fn bilinear_reduce(image: *rl.Image) !rl.Texture {
+	var new = rl.genImageColor(@divFloor(image.width, 2), @divFloor(image.height, 2), .white);
+	
+	var new_colors = try rl.loadImageColors(new);
+	const old_colors = try rl.loadImageColors(image.*);
+	defer rl.unloadImageColors(old_colors);
+	
+	const old_width : usize = @intCast(image.width);
+	const new_width : usize = @intCast(new.width);
+	const new_height : usize = @intCast(new.height);
+	for (0..new_width) |i| {
+		for (0..new_height) |j| {
+			new_colors[j * new_width + i].r = @intCast((@as(usize, @intCast(old_colors[j * old_width * 2 + i * 2].r))
+				+ @as(usize, @intCast(old_colors[j * old_width * 2 + i * 2 + 1].r))
+				+ @as(usize, @intCast(old_colors[j * old_width * 2 + i * 2 + old_width].r))
+				+ @as(usize, @intCast(old_colors[j * old_width * 2 + i * 2 + old_width + 1].r))) / 4);
+
+			new_colors[j * new_width + i].g = @intCast((@as(usize, @intCast(old_colors[j * old_width * 2 + i * 2].g))
+				+ @as(usize, @intCast(old_colors[j * old_width * 2 + i * 2 + 1].g))
+				+ @as(usize, @intCast(old_colors[j * old_width * 2 + i * 2 + old_width].g))
+				+ @as(usize, @intCast(old_colors[j * old_width * 2 + i * 2 + old_width + 1].g))) / 4);
+
+			new_colors[j * new_width + i].b = @intCast((@as(usize, @intCast(old_colors[j * old_width * 2 + i * 2].b))
+				+ @as(usize, @intCast(old_colors[j * old_width * 2 + i * 2 + 1].b))
+				+ @as(usize, @intCast(old_colors[j * old_width * 2 + i * 2 + old_width].b))
+				+ @as(usize, @intCast(old_colors[j * old_width * 2 + i * 2 + old_width + 1].b))) / 4);
+
+			new_colors[j * new_width + i].a = @intCast((@as(usize, @intCast(old_colors[j * old_width * 2 + i * 2].a))
+				+ @as(usize, @intCast(old_colors[j * old_width * 2 + i * 2 + 1].a))
+				+ @as(usize, @intCast(old_colors[j * old_width * 2 + i * 2 + old_width].a))
+				+ @as(usize, @intCast(old_colors[j * old_width * 2 + i * 2 + old_width + 1].a))) / 4);
+		}
+	}
+	new.data = new_colors.ptr;
+	new.format = .uncompressed_r8g8b8a8;
+
+	return rl.loadTextureFromImage(new);
+}
+
+pub fn bilinear_expand(image: *rl.Image) !rl.Texture {
+	var new = rl.genImageColor(image.width * 2, image.height * 2, .white);
+	
+	var new_colors = try rl.loadImageColors(new);
+	const old_colors = try rl.loadImageColors(image.*);
+	defer rl.unloadImageColors(old_colors);
+	
+	const old_width : usize = @intCast(image.width);
+	const old_height : usize = @intCast(image.height);
+	const new_width : usize = @intCast(new.width);
+	
+	for (0..old_width) |i| {
+		for (0..old_height) |j| {
+			new_colors[j * new_width * 2 + i * 2].r = old_colors[j * old_width + i].r;
+			new_colors[j * new_width * 2 + i * 2].g = old_colors[j * old_width + i].g;
+			new_colors[j * new_width * 2 + i * 2].b = old_colors[j * old_width + i].b;
+			new_colors[j * new_width * 2 + i * 2].a = old_colors[j * old_width + i].a;
+
+			if (i + 1 < old_width) {
+				new_colors[j * new_width * 2 + i * 2 + 1].r = @intCast((@as(usize, @intCast(old_colors[j * old_width + i].r))
+					+ @as(usize, @intCast(old_colors[j * old_width + i + 1].r))) / 2);
+				new_colors[j * new_width * 2 + i * 2 + 1].g = @intCast((@as(usize, @intCast(old_colors[j * old_width + i].g))
+					+ @as(usize, @intCast(old_colors[j * old_width + i + 1].g))) / 2);
+				new_colors[j * new_width * 2 + i * 2 + 1].b = @intCast((@as(usize, @intCast(old_colors[j * old_width + i].b))
+					+ @as(usize, @intCast(old_colors[j * old_width + i + 1].b))) / 2);
+				new_colors[j * new_width * 2 + i * 2 + 1].a = @intCast((@as(usize, @intCast(old_colors[j * old_width + i].a))
+					+ @as(usize, @intCast(old_colors[j * old_width + i + 1].a))) / 2);
+			} else {
+				new_colors[j * new_width * 2 + i * 2 + 1].r = old_colors[j * old_width + i].r;
+				new_colors[j * new_width * 2 + i * 2 + 1].g = old_colors[j * old_width + i].g;
+				new_colors[j * new_width * 2 + i * 2 + 1].b = old_colors[j * old_width + i].b;
+				new_colors[j * new_width * 2 + i * 2 + 1].a = old_colors[j * old_width + i].a;
+			}
 
 			if (j + 1 < old_height) {
-				new_colors[(j + 1) * new_width * 2 + i * 2].r = old_colors[j * old_width + i].r;
-				new_colors[(j + 1) * new_width * 2 + i * 2].g = old_colors[j * old_width + i].g;
-				new_colors[(j + 1) * new_width * 2 + i * 2].b = old_colors[j * old_width + i].b;
-				new_colors[(j + 1) * new_width * 2 + i * 2].a = old_colors[j * old_width + i].a;
+				new_colors[j * new_width * 2 + i * 2 + new_width].r = @intCast((@as(usize, @intCast(old_colors[j * old_width + i].r))
+					+ @as(usize, @intCast(old_colors[j * old_width + i + old_width].r))) / 2);
+				new_colors[j * new_width * 2 + i * 2 + new_width].g = @intCast((@as(usize, @intCast(old_colors[j * old_width + i].g))
+					+ @as(usize, @intCast(old_colors[j * old_width + i + old_width].g))) / 2);
+				new_colors[j * new_width * 2 + i * 2 + new_width].b = @intCast((@as(usize, @intCast(old_colors[j * old_width + i].b))
+					+ @as(usize, @intCast(old_colors[j * old_width + i + old_width].b))) / 2);
+				new_colors[j * new_width * 2 + i * 2 + new_width].a = @intCast((@as(usize, @intCast(old_colors[j * old_width + i].a))
+					+ @as(usize, @intCast(old_colors[j * old_width + i + old_width].a))) / 2);
+			} else {
+				new_colors[j * new_width * 2 + i * 2 + new_width].r = old_colors[j * old_width + i].r;
+				new_colors[j * new_width * 2 + i * 2 + new_width].g = old_colors[j * old_width + i].g;
+				new_colors[j * new_width * 2 + i * 2 + new_width].b = old_colors[j * old_width + i].b;
+				new_colors[j * new_width * 2 + i * 2 + new_width].a = old_colors[j * old_width + i].a;
+			}
 
-				new_colors[(j + 1) * new_width * 2 + (i + 1) * 2].r = old_colors[j * old_width + i].r;
-				new_colors[(j + 1) * new_width * 2 + (i + 1) * 2].g = old_colors[j * old_width + i].g;
-				new_colors[(j + 1) * new_width * 2 + (i + 1) * 2].b = old_colors[j * old_width + i].b;
-				new_colors[(j + 1) * new_width * 2 + (i + 1) * 2].a = old_colors[j * old_width + i].a;
+			if (i + 1 < old_width and j + 1 < old_height) {
+				new_colors[j * new_width * 2 + i * 2 + new_width + 1].r = @intCast((@as(usize, @intCast(old_colors[j * old_width + i].r))
+					+ @as(usize, @intCast(old_colors[j * old_width + i + 1].r))
+					+ @as(usize, @intCast(old_colors[j * old_width + i + old_width].r))
+					+ @as(usize, @intCast(old_colors[j * old_width + i + old_width + 1].r))) / 4);
+				new_colors[j * new_width * 2 + i * 2 + new_width + 1].g = @intCast((@as(usize, @intCast(old_colors[j * old_width + i].g))
+					+ @as(usize, @intCast(old_colors[j * old_width + i + 1].g))
+					+ @as(usize, @intCast(old_colors[j * old_width + i + old_width].g))
+					+ @as(usize, @intCast(old_colors[j * old_width + i + old_width + 1].g))) / 4);
+				new_colors[j * new_width * 2 + i * 2 + new_width + 1].b = @intCast((@as(usize, @intCast(old_colors[j * old_width + i].b))
+					+ @as(usize, @intCast(old_colors[j * old_width + i + 1].b))
+					+ @as(usize, @intCast(old_colors[j * old_width + i + old_width].b))
+					+ @as(usize, @intCast(old_colors[j * old_width + i + old_width + 1].b))) / 4);
+				new_colors[j * new_width * 2 + i * 2 + new_width + 1].a = @intCast((@as(usize, @intCast(old_colors[j * old_width + i].a))
+					+ @as(usize, @intCast(old_colors[j * old_width + i + 1].a))
+					+ @as(usize, @intCast(old_colors[j * old_width + i + old_width].a))
+					+ @as(usize, @intCast(old_colors[j * old_width + i + old_width + 1].a))) / 4);
+			} else {
+				new_colors[j * new_width * 2 + i * 2 + new_width + 1].r = old_colors[j * old_width + i].r;
+				new_colors[j * new_width * 2 + i * 2 + new_width + 1].g = old_colors[j * old_width + i].g;
+				new_colors[j * new_width * 2 + i * 2 + new_width + 1].b = old_colors[j * old_width + i].b;
+				new_colors[j * new_width * 2 + i * 2 + new_width + 1].a = old_colors[j * old_width + i].a;
 			}
 		}
 	}
